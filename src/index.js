4821e7d59b4ec566d591229ce6240a0e44489fcb
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-09-19 14:45:59 -0400

    planning and store boilerplate

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom'
 import './assets/css/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// App
+//   NavBar
+//   SearchBar (local state)
+//   BookSearchList
+//     Book
+//   Bookshelf
+//     Book
+//   BookDetail
+
+// Initial State: {
+//   searchResults: [],
+//   selectedBook: {},
+//   bookshelfBooks: []
+// }
+
+function reducer(state, action) {
+  switch (action.type) {
+    // case expression:
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
 registerServiceWorker()
