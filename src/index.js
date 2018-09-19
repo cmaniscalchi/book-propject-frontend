df42658013733b892bd3c70bd7459b8351ed9339
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-09-19 14:58:46 -0400

    redux restructured & initial state added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import './assets/css/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import store from './store'
 
 // App
 //   NavBar
@@ -14,22 +14,6 @@ import { Provider } from 'react-redux'
 //   Bookshelf
 //     Book
 //   BookDetail
-
-// Initial State: {
-//   searchResults: [],
-//   selectedBook: {},
-//   bookshelfBooks: []
-// }
-
-function reducer(state, action) {
-  switch (action.type) {
-    // case expression:
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer)
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
 registerServiceWorker()
---
+++ src/reducers/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  searchResults: [],
+  selectedBook: {},
+  bookshelfBooks: []
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    // case expression:
+    // return object
+    default:
+      return state
+  }
+}
