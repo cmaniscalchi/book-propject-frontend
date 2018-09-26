7b09c8005c099f4ab2c2bac94060c1cab64a826e
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-09-25 20:08:25 -0400

    preventing users from saving a book more than once to a shelf

--- src/components/BookSearchDetail.js
+++ src/components/BookSearchDetail.js
@@ -1,29 +1,42 @@
-import React from "react"
+import React, { Component } from "react"
 import { connect } from 'react-redux'
 import { saveBook, deleteBook, viewEditions } from '../actions'
 import { Container, Button } from 'semantic-ui-react'
+import { setShelvedBooks } from '../actions'
 
-const BookSearchDetail = ({ shelvedBooks, book, saveBook, userId }) => {
-  console.log("BookSearchDetail:", shelvedBooks, book, userId)
+class BookSearchDetail extends Component {
 
-  if (book) {
-    return (
-      <Container>
-        <h1>{book.title} by {book.author}</h1>
-        <Button onClick={() => saveBook(book, userId)}>Save Book to Bookshelf</Button>
-      </Container>
-    )
-  } else {
-  return null
+  componentDidMount() {
+    let { shelvedBooks, setShelvedBooks } = this.props
+    console.log(shelvedBooks)
+    setShelvedBooks(shelvedBooks)
+  }
+
+  render() {
+    let { book, userId, saveBook, shelvedBooks } = this.props
+    // console.log("BookSearchDetail:", shelvedBooks, book, userId)
+
+    if (book) {
+      debugger
+      return (
+        <Container>
+          <h1>{book.title} by {book.author}</h1>
+          {shelvedBooks.some(shelvedBook => shelvedBook.goodreads_book_id === book.goodreads_book_id) ? <em>This book is already on your bookshelf</em> : <Button onClick={() => saveBook(book, userId)}>Save Book to Bookshelf</Button>}
+        </Container>
+      )
+    } else {
+      return null
+    }
   }
 }
 
 function mapStateToProps(state) {
+  console.log(state)
   return {
     book: state.book.selectedBook,
-    shelvedBooks: state.book.shelvedBooks,
+    shelvedBooks: state.user.user.books,
     userId: state.user.user.id
   }
 }
 
-export default connect(mapStateToProps, { saveBook, deleteBook, viewEditions })(BookSearchDetail)
+export default connect(mapStateToProps, { setShelvedBooks, saveBook })(BookSearchDetail)
