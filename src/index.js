9603253658a3a35845b486461ae6112b7850e14b
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-11-28 21:50:55 -0500

    incorporating dotenv package for better handling of environment variables

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+require('dotenv').config()
+
 
 ReactDOM.render(
   <Provider store={store}>
