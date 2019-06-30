212ace2a0a9b3fa52f94d4ab6ad9eceeb66242d5
cmaniscalchi <claudia.maniscalchi@gmail.com>
2019-06-30 12:17:22 -0400

    adding eslint & cleaning up BookshelfList

--- src/components/BookshelfList.js
+++ src/components/BookshelfList.js
@@ -6,36 +6,47 @@ import { clearSearchResults, clearSelectedBook, clearSelectedCover, setDefaultBo
 
 class BookshelfList extends Component {
 
-  componentDidMount() {
-    let { clearSelectedBook, clearSelectedCover, currentBookshelf, setDefaultBookshelf } = this.props
-    if (!currentBookshelf) {
-      setDefaultBookshelf()
-      clearSelectedBook()
-      clearSelectedCover()
-      clearSearchResults()
-    } else {
-      clearSelectedBook()
-      clearSelectedCover()
-      clearSearchResults()
-    }
-  }
+componentDidMount() {
 
-  render() {
-    // console.log("BookshelfList props:", this.props)
-    let { user, bookCovers, currentBookshelf } = this.props
-    if (currentBookshelf) {
-      return (
-        <div>
-          <Grid relaxed columns={4}>
-            {user.books.length > 0 && bookCovers.length === 0 ? user.books.filter(book => book.bookshelf_id === currentBookshelf.id).sort((bookA, bookB) => bookA.publication_year - bookB.publication_year).map(book => <BookshelfBook book={book} key={book.goodreads_book_id} />) : null}
-            {user.books.length > 0 && bookCovers.length > 0 ? bookCovers.map(cover => <BookshelfBook cover={cover} key={cover.imageId} />) : null}
-          </Grid>
-        </div>
-      )
-    } else {
-      return null
+    const { clearSelectedBook, clearSelectedCover, currentBookshelf, setDefaultBookshelf } = this.props;
+
+    clearSelectedBook()
+    clearSelectedCover()
+    clearSearchResults()
+
+    if (!currentBookshelf) {
+        setDefaultBookshelf()
     }
-  }
+}
+
+    render() {
+
+        const { user, bookCovers, currentBookshelf } = this.props;
+
+        return !!currentBookshelf && !!user.books.length && (
+
+            <Grid relaxed columns={4}>
+
+                {!bookCovers.length && user.books
+                    .filter(book => book.bookshelf_id === currentBookshelf.id)
+                    .sort((bookA, bookB) => bookA.publication_year - bookB.publication_year)
+                    .map(book => (
+
+                        <BookshelfBook book={book} key={book.goodreads_book_id} />
+
+                    ))
+                }
+
+                {!!bookCovers.length && bookCovers.map(cover => (
+
+                    <BookshelfBook cover={cover} key={cover.imageId} />
+
+                ))}
+
+            </Grid>
+
+        );
+    }
 }
 
 const mapStateToProps = ({ book: { bookCovers }, user: { currentBookshelf, user } }) => ({ bookCovers, user, currentBookshelf })
